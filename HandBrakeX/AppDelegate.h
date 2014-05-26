ffc3febd28c5acf05aa5760d52b6229780644ec8
JinMingTao <admin@JingMingTaos-Mac.local>
2014-05-26 20:26:55 +0900

    1

--- HandBrakeX/AppDelegate.h
+++ HandBrakeX/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate, NSTableViewDataSource, NSTableViewDelegate, MovieConverterDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
 
