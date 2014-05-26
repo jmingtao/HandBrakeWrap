68339c86d6f89dbcaa1f9b7599e09b42fe627f95
JinMingTao <admin@JingMingTaos-Mac.local>
2014-05-26 20:24:11 +0900

    Initial Commit

---
+++ HandBrakeX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HandBrakeX
+//
+//  Created by JinMingTao on 5/26/14.
+//  Copyright (c) 2014 jinmingtao. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
